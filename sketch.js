d91172f1a41e93204ab654cf85858c06e35a389f
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-10-22 18:38:19 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 let button;
-
+let bg;
 
 function setup() {
-  button = createButton('Button');
-
+  noCanvas();
+  button = createButton('Change Background');
+  button.position(width/2, height/2);
+  button.mousePressed(buttonPressed);
+  bg = createElement('body');
 }
 
 function draw() {
-  
+
+
+function buttonPressed() {
+  let body;
+  body = select('body');
+  body.style('background-image', "url('images/microsoft_surface_wallpaper_2.jpg')");
 }
